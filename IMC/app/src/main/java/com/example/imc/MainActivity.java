59673b8440ee47bb623b0c3949ba80acde55a5d6
Isaque <isaquejpadilha.07@gmail.com>
2024-09-30 08:31:28 -0300

    dadasdsad

---
+++ IMC/app/src/main/java/com/example/imc/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("ciclo de vida","onCreate");
+        Button button = findViewById(R.id.button);
+        EditText a = findViewById(R.id.txtAltura);
+        EditText p = findViewById(R.id.txtPeso);
+
+
+        //Button button = findViewById(R.id.btnFechar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),atividadeB.class);
+                //i.putExtra("msg","oi");
+                float h = Float.parseFloat(a.getText().toString());
+                float w = Float.parseFloat(p.getText().toString());
+                i.putExtra("altura",a.getText().toString());
+                i.putExtra("peso",p.getText().toString());
+
+                startActivity(i);
+            }
+        });
+    }
+    protected void onStart(){
+        super.onStart();
+        Log.d("ciclo de vida", "onStart");
+    }
+    protected void onResume(){
+        super.onResume();
+        Log.d("ciclo de vida", "onResume");
+    }
+    protected void onPause(){
+        super.onPause();
+        Log.d("ciclo de vida","onPause");
+    }
+}
---
+++ IMC/app/src/main/java/com/example/imc/atividadeB.java
@@ -0,0 +1,80 @@
+package com.example.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+
+public class atividadeB extends AppCompatActivity {
+    float weight,height;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_b);
+        TextView peso = findViewById(R.id.lblPeso);
+        TextView altura = findViewById(R.id.lblAltura);
+        TextView imc = findViewById(R.id.lblIMC);
+        TextView resultado = findViewById(R.id.lblResultado);
+
+        weight = getIntent().getExtras().getFloat("peso");
+        height = getIntent().getExtras().getFloat("altura");
+        Float total = weight/(height*height);
+
+        DecimalFormat df = new DecimalFormat(("##,##"));
+        altura.setText(df.format(height)+" m");
+        peso.setText(df.format(weight)+" Kg");
+        imc.setText(df.format(total));
+        setPerfil(total);
+
+
+
+
+
+    }
+    public void setPerfil(Float imc){
+        ImageView image = findViewById(R.id.imageView);
+        TextView result = findViewById(R.id.lblIMC);
+        if (imc<=18.5){
+            result.setText("Abaixo do Peso");
+            image.setImageResource(R.drawable.abaixopeso);
+        }else{
+            if(imc<=24.9){
+                result.setText("Normal");
+                image.setImageResource(R.drawable.normal);
+            }
+            else{
+                if(imc<=29.9){
+                    result.setText("Sobrepeso");
+                    image.setImageResource(R.drawable.sobrepeso);
+                }
+                else{
+                    if(imc<=34.9){
+                        result.setText("Obesidade grau 1");
+                        image.setImageResource(R.drawable.obesidade1);
+                    }
+                    else{
+                        if(imc<=39.9){
+                            result.setText("Obesidade grau 2");
+                            image.setImageResource(R.drawable.obesidade2);
+                        }
+                        else{
+                            if(imc>=40){
+                                result.setText("Obesidade grau 3");
+                                image.setImageResource(R.drawable.obesidade3);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
